52cfea128894eeee2e579b97dce7d2eb4739ecc5
EndijaM <115448772+EndijaM@users.noreply.github.com>
2022-11-03 11:25:00 +0100

    Displaying current temperature

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "8d9838178b5b401f1b4e7cb5af18e210";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Luxembourg&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
